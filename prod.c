feaaec9092d7c4ecb35cd423160d1cd96c5d8ea8
maithaalhayyas <maithaalhayyas@gmail.com>
2021-06-28 11:01:09 +0400

    my_greeter12 function that print the sum of two integers recieved by user

--- prod.c
+++ prod.c
@@ -8,13 +8,3 @@ int my_greeter12(void) {
     int number1, number2;
     int Sum;
 
-
-    printf("1st number:");
-    scanf("%d", number1);
-    printf("2nd number:");
-    scanf("%d", number2);
-    Sum = number1 + number2;
-
-    return (Sum);
-
-}
