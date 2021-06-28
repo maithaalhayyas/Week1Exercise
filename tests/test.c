83fb27bb5a62679a754cffb514b4961f829e9d80
maithaalhayyas <maithaalhayyas@gmail.com>
2021-06-28 09:58:26 +0400

    Canary test always shall be true

--- tests/test.c
+++ tests/test.c
@@ -6,16 +6,19 @@
 
 #include "../prod.h"
 
+int variable(int a , int b ){
+    return (a + b);
+}
 
 static void Sum(void **state){
 
-     int a;
-    int b;
-    assert_int_equal(0, 0);
+    int a ={1};
+    int b ={2};
+    assert_int_equal(variable( a, b), variable( a, b));
 
 
 }
-/* A test case that does nothing and succeeds. - Declaring Tests */
+/* A test case that does nothing and succeeds. */
 static void canary_test(void **state) {
     (void) state; /* unused */
     assert_int_equal(0, 0);
