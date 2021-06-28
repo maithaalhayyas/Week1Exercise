dfc23b2787bd5f4a9c50423cdf37ea91bb14da03
maithaalhayyas <maithaalhayyas@gmail.com>
2021-06-28 11:01:56 +0400

    Recieve the two integers from the user

--- prod.c
+++ prod.c
@@ -8,3 +8,14 @@ int my_greeter12(void) {
     int number1, number2;
     int Sum;
 
+    printf("1st number:");
+    scanf("%d", number1);
+    printf("2nd number:");
+    scanf("%d", number2);
+    Sum = number1 + number2;
+
+    return (Sum);
+
+}
+
+
